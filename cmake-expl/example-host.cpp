8a2a80296313198211178f376460e58410a6b6c7
Laurentiu Stoleriu <lstoler@staff.uaic.ro>
2023-08-13 19:20:33 +0300

    paralel?

--- cmake-expl/example-host.cpp
+++ cmake-expl/example-host.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <random>
 #include <TNL/Containers/Vector.h>
 #include <TNL/Containers/StaticArray.h>
 #include <TNL/Algorithms/parallelFor.h>
@@ -7,27 +8,32 @@ using namespace TNL;
 using namespace TNL::Containers;
 using namespace TNL::Algorithms;
 
-template< typename Device >
-void initMeshFunction( const int xSize, const int ySize, Vector< double, Device > &v, const double &c)
+template<typename Device>
+void initMeshFunction (const int xSize, const int ySize, Vector<double, Device> &v, const double &c)
 {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(100.0, 200.0);
+
     auto view = v.getView();
-    auto init = [=] __cuda_callable__ ( const StaticArray< 2, int > &i ) mutable
+    auto init = [=] __cuda_callable__ (const StaticArray<2, int> &i) mutable
     {
-        view[ i.y()  * xSize + i.x() ] = c;
+        view[i.y()  * xSize + i.x()] = dis(gen)*dis(gen)*dis(gen)/dis(gen);
     };
-    StaticArray< 2, int > begin{ 0, 0, 0 };
-    StaticArray< 2, int > end{ xSize, ySize };
-    parallelFor< Device >( begin, end, init );
+    StaticArray<2, int> begin{0, 0};
+    StaticArray<2, int> end{xSize, ySize};
+    parallelFor<Device>(begin, end, init);
 }
 
-int main( int argc, char* argv[] )
+int main(int argc, char* argv[])
 {
-    const int xSize( 10 ), ySize( 10 );
+    const int xSize(10000), ySize(10000);
     const int size = xSize * ySize;
 
-    Vector< double, Devices::Host > host_v( size );
-    initMeshFunction( xSize, ySize, host_v, 1.0 );
-    std::cout << "host_v = " << host_v << std::endl;
+    Vector<double, Devices::Host> host_v(size);
+    initMeshFunction(xSize, ySize, host_v, 1.0);
+    //std::cout << "host_v = " << host_v << std::endl;
+    std::cout << "DONE!!" << std::endl;
 
 #ifdef __CUDACC__
     Vector< double, Devices::Cuda > cuda_v( size );

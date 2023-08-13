829f67a90f74171102944568a15a8e182dc8b1f9
Laurentiu Stoleriu <lstoler@staff.uaic.ro>
2023-08-13 06:32:47 +0300

    ParallelFor example

--- cmake-expl/example-host.cpp
+++ cmake-expl/example-host.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
-#include <TNL/Containers/Array.h>
+#include <TNL/Containers/Vector.h>
+#include <TNL/Algorithms/parallelFor.h>
+
+using namespace TNL;
+using namespace TNL::Containers;
+using namespace TNL::Algorithms;
+
+/****
+ * Set all elements of the vector v to the constant c.
+ */
+template< typename Device >
+void initVector( Vector< double, Device > &v, const double &c )
+{
+    auto view = v.getView();
+    auto init = [=] __cuda_callable__ ( int i ) mutable
+    {
+        view[ i ] = c;
+    };
+    parallelFor< Device >( 0, v.getSize(), init );
+}
 
 int main( int argc, char* argv[] )
 {
-    TNL::Containers::Array< int > host_array{ 1, 2, 3 };
-    std::cout << "host_array = " << host_array << std::endl;
+    Vector< double, Devices::Host > host_v( 10 );
+    initVector( host_v, 1.0 );
+    std::cout << "host_v = " << host_v << std::endl;
+
+#ifdef __CUDACC__
+    Vector< double, Devices::Cuda > cuda_v( 10 );
+    initVector( cuda_v, 1.0 );
+    std::cout << "cuda_v = " << cuda_v << std::endl;
+#endif
     return EXIT_SUCCESS;
 }

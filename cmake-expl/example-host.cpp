c42be880a2b9fa78003d5353a3b2bd4e270ac351
Laurentiu Stoleriu <lstoler@staff.uaic.ro>
2023-08-13 06:39:36 +0300

    2D PArallelFor OK

--- cmake-expl/example-host.cpp
+++ cmake-expl/example-host.cpp
@@ -8,30 +8,30 @@ using namespace TNL::Containers;
 using namespace TNL::Algorithms;
 
 template< typename Device >
-void initMeshFunction( const int xSize, const int ySize, const int zSize, Vector< double, Device > &v, const double &c)
+void initMeshFunction( const int xSize, const int ySize, Vector< double, Device > &v, const double &c)
 {
     auto view = v.getView();
-    auto init = [=] __cuda_callable__ ( const StaticArray< 3, int > &i ) mutable
+    auto init = [=] __cuda_callable__ ( const StaticArray< 2, int > &i ) mutable
     {
-        view[ ( i.z() * ySize + i.y() ) * xSize + i.x() ] = c;
+        view[ i.y()  * xSize + i.x() ] = c;
     };
-    StaticArray< 3, int > begin{ 0, 0, 0 };
-    StaticArray< 3, int > end{ xSize, ySize, zSize };
+    StaticArray< 2, int > begin{ 0, 0, 0 };
+    StaticArray< 2, int > end{ xSize, ySize };
     parallelFor< Device >( begin, end, init );
 }
 
 int main( int argc, char* argv[] )
 {
-    const int xSize( 10 ), ySize( 10 ), zSize( 10 );
-    const int size = xSize * ySize * zSize;
+    const int xSize( 10 ), ySize( 10 );
+    const int size = xSize * ySize;
 
     Vector< double, Devices::Host > host_v( size );
-    initMeshFunction( xSize, ySize, zSize, host_v, 1.0 );
+    initMeshFunction( xSize, ySize, host_v, 1.0 );
     std::cout << "host_v = " << host_v << std::endl;
 
 #ifdef __CUDACC__
     Vector< double, Devices::Cuda > cuda_v( size );
-    initMeshFunction( xSize, ySize, zSize, cuda_v, 1.0 );
+    initMeshFunction( xSize, ySize, cuda_v, 1.0 );
 #endif
 
     return EXIT_SUCCESS;
